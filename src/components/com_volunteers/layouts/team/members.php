0e56af2df73fa0fd14cc0955432c4fa8482a602b
Brian Teeman <brian@teeman.net>
2023-05-02 13:06:09 +0100

    refactor layout (#3)

--- src/components/com_volunteers/layouts/team/members.php
+++ src/components/com_volunteers/layouts/team/members.php
@@ -8,92 +8,104 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
-use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\CMS\Router\Route;
 use Joomla\Component\Volunteers\Site\Helper\VolunteersHelper;
 
 defined('_JEXEC') or die('Restricted access');
 /* @var $displayData array */
+?>
 
-if ($displayData->item->active) {
-    echo HTMLHelper::_('uitab.addTab', 'teamsTab', 'viewmembers', Text::_('COM_VOLUNTEERS_TAB_MEMBERS'));
-    if ($displayData->acl->edit) {
-        echo '<div class="row-fluid">
-        <a class="btn pull-right" href="' . Route::_('index.php?option=com_volunteers&task=member.add&team=' . $displayData->item->id) . '">
-            <span class="icon-new" aria-hidden="true"></span> ' . Text::_('COM_VOLUNTEERS_MEMBER_ADD') . '
-        </a>
-    </div>
-    <hr>';
-    }
-    ;
-    if ($displayData->item->members->active) {
-        echo '<table class="table table-striped table-hover table-vertical-align">
-    <thead>
-    <th width="30%">' . Text::_('COM_VOLUNTEERS_FIELD_VOLUNTEER') . '</th>
-    <th width="20%">' . Text::_('COM_VOLUNTEERS_FIELD_POSITION') . '</th>
-    <th>' . Text::_('COM_VOLUNTEERS_FIELD_ROLE') . '</th>
-    <th width="12%">' . Text::_('COM_VOLUNTEERS_FIELD_DATE_STARTED') . '</th>';
-        if ($displayData->acl->edit) {
-            echo '<th width="10%" class="center">' . Text::_('COM_VOLUNTEERS_FIELD_ADDRESS') . '</th>
-        <th width="10%" class="center">' . Text::_('COM_VOLUNTEERS_FIELD_NDA') . '</th>
-        <th width="10%"></th>';
-        }
-        ;
-        echo '</thead>';
-        echo '<tbody>';
-        foreach ($displayData->item->members->active as $volunteer) {
-            echo '<tr>
-            <td class="volunteer-image">
-                <a class="pull-left"
-                   href="' . Route::_('index.php?option=com_volunteers&view=volunteer&id=' . $volunteer->volunteer) . '">' .
-                VolunteersHelper::image($volunteer->volunteer_image, 'small', false, $volunteer->volunteer_image) . '
-                </a>
-                <a href="' . Route::_('index.php?option=com_volunteers&view=volunteer&id=' . $volunteer->volunteer) . '">' .
-                $volunteer->volunteer_name . '
-                </a>
-                <span class="muted volunteer-location">
-                                            <span class="icon-location" aria-hidden="true"></span>' . VolunteersHelper::location($volunteer->volunteer_country) . '
-                                        </span>
-            </td>
-            <td>' . $volunteer->position_title . '
-            </td>
-            <td>' .
-                $volunteer->role_title . '
-            </td>
-            <td>' . VolunteersHelper::date($volunteer->date_started, 'M Y') . '
-            </td>';
-            if ($displayData->acl->edit) {
-                echo '<td class="center">';
-                if ($volunteer->address) {
-                    echo ' <span class="icon-checkbox-checked"></span>';
-                } else {
-                    echo ' <span class="icon-checkbox-unchecked"></span>';
-                }
-                ;
-                echo '</td>
-                <td class="center">';
-                if ($volunteer->nda) {
-                    echo '<span class="icon-checkbox-checked"></span>';
-                } else {
-                    echo '<span class="icon-checkbox-unchecked"></span>';
-                }
-                ;
-                echo '</td>
-                <td>
-                    <a class="btn btn-small pull-right" href="' . Route::_('index.php?option=com_volunteers&task=member.edit&id=' . $volunteer->id) . '">
-                        <span class="icon-edit" aria-hidden="true"></span> ' . Text::_('COM_VOLUNTEERS_EDIT') . '
-                    </a>
-                </td>';
-            }
-            ;
-            echo '</tr>';
-        }
-        echo '</tbody>
-</table>';
-    }
-    ;
-}
-;
+<?php if ($displayData->item->active) { ?>
+    <?php echo HTMLHelper::_('uitab.addTab', 'teamsTab', 'viewmembers', Text::_('COM_VOLUNTEERS_TAB_MEMBERS')); ?>
+    <?php if ($displayData->acl->edit) { ?>
+        <div class="row-fluid">
+            <a class="btn pull-right"
+                href="<?php echo Route::_('index.php?option=com_volunteers&task=member.add&team=' . $displayData->item->id); ?>">
+                <span class="icon-new" aria-hidden="true"></span>
+                <?php echo Text::_('COM_VOLUNTEERS_MEMBER_ADD'); ?>
+            </a>
+        </div>
+        <hr>
+    <?php } ?>
+    <?php if ($displayData->item->members->active): ?>
+        <table class="table table-striped table-hover table-vertical-align">
+            <thead>
+                <th width="30%">
+                    <?php echo Text::_('COM_VOLUNTEERS_FIELD_VOLUNTEER'); ?>
+                </th>
+                <th width="20%">
+                    <?php echo Text::_('COM_VOLUNTEERS_FIELD_POSITION'); ?>
+                </th>
+                <th>
+                    <?php echo Text::_('COM_VOLUNTEERS_FIELD_ROLE'); ?>
+                </th>
+                <th width="12%">
+                    <?php echo Text::_('COM_VOLUNTEERS_FIELD_DATE_STARTED'); ?>
+                </th>
+                <?php if ($displayData->acl->edit): ?>
+                    <th width="10%" class="center">
+                        <?php echo Text::_('COM_VOLUNTEERS_FIELD_ADDRESS'); ?>
+                    </th>
+                    <th width="10%" class="center">
+                        <?php echo Text::_('COM_VOLUNTEERS_FIELD_NDA'); ?>
+                    </th>
+                    <th width="10%"></th>
+                <?php endif; ?>
+            </thead>
+            <tbody>
+                <?php foreach ($displayData->item->members->active as $volunteer): ?>
+                    <tr>
+                        <td class="volunteer-image">
+                            <a class="pull-left"
+                                href="<?php echo Route::_('index.php?option=com_volunteers&view=volunteer&id=' . $volunteer->volunteer); ?>">
+                                <?php echo VolunteersHelper::image($volunteer->volunteer_image, 'small', false, $volunteer->volunteer_image); ?>
+                            </a>
+                            <a
+                                href="<?php echo Route::_('index.php?option=com_volunteers&view=volunteer&id=' . $volunteer->volunteer); ?>">
+                                <?php echo $volunteer->volunteer_name; ?>
+                            </a>
+                            <span class="muted volunteer-location">
+                                <span class="icon-location" aria-hidden="true"></span>
+                                <?php echo VolunteersHelper::location($volunteer->volunteer_country); ?>
+                            </span>
+                        </td>
+                        <td>
+                            <?php echo $volunteer->position_title; ?>
+                        </td>
+                        <td>
+                            <?php echo $volunteer->role_title; ?>
+                        </td>
+                        <td>
+                            <?php echo VolunteersHelper::date($volunteer->date_started, 'M Y'); ?>
+                        </td>
+                        <?php if ($displayData->acl->edit): ?>
+                            <td class="center">
+                                <?php if ($volunteer->address) { ?>
+                                    <span class="icon-checkbox-checked"></span>
+                                <?php } else { ?>
+                                    <span class="icon-checkbox-unchecked"></span>
+                                <?php } ?>
+                            </td>
+                            <td class="center">
+                                <?php if ($volunteer->nda) { ?>
+                                    <span class="icon-checkbox-checked"></span>
+                                <?php } else { ?>
+                                    <span class="icon-checkbox-unchecked"></span>
+                                <?php } ?>
+                            </td>
+                            <td>
+                                <a class="btn btn-small pull-right"
+                                    href="<?php echo Route::_('index.php?option=com_volunteers&task=member.edit&id=' . $volunteer->id); ?>">
+                                    <span class="icon-edit" aria-hidden="true"></span>
+                                    <?php echo Text::_('COM_VOLUNTEERS_EDIT'); ?>
+                                </a>
+                            </td>
+                        <?php endif; ?>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php endif; ?>
+<?php } ?>
