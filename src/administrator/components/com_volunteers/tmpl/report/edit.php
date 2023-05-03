051ae92b5df655317b0996a537b24d1f2a80e854
brian teeman <brian@teeman.net>
2023-05-03 15:33:07 +0100

    admin reports

--- src/administrator/components/com_volunteers/tmpl/report/edit.php
+++ src/administrator/components/com_volunteers/tmpl/report/edit.php
@@ -6,33 +6,44 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Layout\LayoutHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
+
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
 // phpcs:enable PSR1.Files.SideEffects
+/** @var $this HtmlView */
 
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Router\Route;
-
-HTMLHelper::_('behavior.formvalidator');
-HTMLHelper::_('formbehavior.chosen', 'select');
-
+$wa = $this->document->getWebAssetManager();
+$wa->useScript('keepalive')
+    ->useScript('form.validate');
 ?>
 
-<form action="<?php echo Route::_('index.php?option=com_volunteers&layout=edit&id=' . (int) $this->item->id); ?>" method="post" name="adminForm" id="report-form" class="form-validate">
+<form action="<?php echo Route::_('index.php?option=com_volunteers&layout=edit&id=' . (int) $this->item->id); ?>"
+    method="post" name="adminForm" id="team-form" class="form-validate">
+    <?php echo LayoutHelper::render('joomla.edit.title_alias', $this); ?>
+    <div class="main-card">
+        <?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', ['active' => 'general', 'recall' => true, 'breakpoint' => 768]); ?>
 
-    <?php echo JLayoutHelper::render('joomla.edit.title_alias', $this); ?>
-
-    <hr>
-
-    <div class="row">
-        <div class="span9">
-            <?php echo $this->form->renderFieldset('item'); ?>
-        </div>
-        <div class="span3">
-            <?php echo $this->form->renderFieldset('details'); ?>
+        <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'general', Text::_('JDETAILS')); ?>
+        <div class="row">
+            <div class="col-lg-9">
+                <div>
+                    <fieldset class="adminform">
+                        <?php echo $this->form->renderFieldset('item'); ?>
+                    </fieldset>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <?php echo $this->form->renderFieldset('details'); ?>
+            </div>
         </div>
     </div>
+    <?php echo HTMLHelper::_('uitab.endTab'); ?>
 
-    <input type="hidden" name="task" value=""/>
+    <?php echo HTMLHelper::_('uitab.endTabSet'); ?>
+    <input type="hidden" name="task" value="" />
     <?php echo HTMLHelper::_('form.token'); ?>
 </form>
