67abd1eb305093f2a73958f6b774eeae3d6cf685
Mark Fleeson <mark@lindisfarne-scriptorium.co.uk>
2023-05-15 13:55:10 +0100

    Adding Faq View
    Adding a view for Faq as article displays quite differently.

---
+++ src/components/com_volunteers/src/Model/FaqModel.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @package    Com_Volunteers
+ * @version    4.0.0
+ * @author     The Joomla Project <secretary@opensourcematters.org>
+ * @copyright  2023 The Joomla Project
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Component\Volunteers\Site\Model;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+use Exception;
+use Joomla\CMS\MVC\Model\ListModel;
+use Joomla\CMS\Router\Route;
+use Joomla\Component\Volunteers\Site\Helper\VolunteersHelper;
+
+/**
+ * Home model.
+ *
+ * @since  4.0.0
+ */
+class FaqModel extends ListModel
+{
+
+}
